5ec51ffa41f8a73f61b61f54a1ef0369dfa18793
Sp21-Bcs-102-Section-B <144018202+Laiba-Basharat@users.noreply.github.com>
2023-10-23 17:26:53 +0500

    Update mid.js

--- sp21-bcs-102 mid exam final/mid.js
+++ sp21-bcs-102 mid exam final/mid.js
@@ -1,1 +1,22 @@
 
+console.log("hello")
+function getlogo() {
+    console.log("getlogo")
+	var logo = document.getElementById("logo");
+	logo.onclick = removenavbar; // here function's refernce is passed function itself is not called
+}
+
+
+function removenavbar() {
+	console.log("remove navbar");
+    document.getElementById("navbar").remove;
+}
+
+
+
+function remove() {
+    var element = document.getElementById("navbar");
+    //I have also tried using document.getElementByID(this.id)
+    element.remove();
+    //I have also tried using element.parentNode.removeChild(element); to remove the element.
+  }
